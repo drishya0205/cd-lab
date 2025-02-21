6676616d7574494ca75bfd311c38900f1469f286
drishya0205 <118071796+drishya0205@users.noreply.github.com>
2025-02-21 21:29:36 +0530

    Update l4aq2.c

--- l4aq2.c
+++ l4aq2.c
@@ -1,7 +1,8 @@
+//this code is made for a sample ruby code
 #include <stdio.h>
-#include <ctype.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #define MAX_FILE_SIZE 10000
 #define MAX_SYMBOLS 500
@@ -18,17 +19,17 @@ typedef enum {
 typedef struct {
     char name[50];
     tokenTypes type;
-    int rowNum;
-    int colNum;
+    int row, col;
 } SymbolTableEntry;
 
 SymbolTableEntry symbolTable[MAX_SYMBOLS];
 int symbolCount = 0;
+int row = 1, col = 1;
 
-const char *keywords[] = {
-    "def", "do", "end", "if", "else", "elsif", "while", "return", "puts", "each"
-};
+const char *keywords[] = {"def", "for", "in", "return", "end", "puts"};
+const char *operators[] = {"=>", "+=", "..", "="};
 
+// Function to check if a token is a keyword
 int isKeyword(const char *str) {
     for (int i = 0; i < sizeof(keywords) / sizeof(keywords[0]); i++) {
         if (strcmp(str, keywords[i]) == 0) return 1;
@@ -36,66 +37,89 @@ int isKeyword(const char *str) {
     return 0;
 }
 
-void addToSymbolTable(const char *token, tokenTypes type, int rowNum, int colNum) {
+// Function to check if a token is an operator
+int isOperator(const char *str) {
+    for (int i = 0; i < sizeof(operators) / sizeof(operators[0]); i++) {
+        if (strcmp(str, operators[i]) == 0) return 1;
+    }
+    return 0;
+}
+
+// Function to add token to symbol table
+void addToSymbolTable(const char *token, tokenTypes type, int row, int col) {
     for (int i = 0; i < symbolCount; i++) {
         if (strcmp(symbolTable[i].name, token) == 0) return;
     }
     if (symbolCount < MAX_SYMBOLS) {
         strncpy(symbolTable[symbolCount].name, token, sizeof(symbolTable[symbolCount].name) - 1);
-        symbolTable[symbolCount].name[sizeof(symbolTable[symbolCount].name) - 1] = '\0';
         symbolTable[symbolCount].type = type;
-        symbolTable[symbolCount].rowNum = rowNum;
-        symbolTable[symbolCount].colNum = colNum;
+        symbolTable[symbolCount].row = row;
+        symbolTable[symbolCount].col = col;
         symbolCount++;
     }
 }
 
+// Tokenizer function
 void getNextToken(FILE *fp) {
-    char bufferFile[MAX_FILE_SIZE];
-    size_t bytesRead = fread(bufferFile, 1, MAX_FILE_SIZE - 1, fp);
-    bufferFile[bytesRead] = '\0';
+    char buffer[MAX_FILE_SIZE];
+    size_t bytesRead = fread(buffer, 1, MAX_FILE_SIZE - 1, fp);
+    buffer[bytesRead] = '\0'; // Null-terminate the buffer
 
-    char *token = strtok(bufferFile, " \t\n{};(),[]");
-    int rowNum = 1, colNum = 1;
+    char *token = strtok(buffer, " \t\n{};(),[]");
 
     while (token) {
-        int startCol = colNum;
-        if (strncmp(token, "#!", 2) == 0) {
-            token = strtok(NULL, "\n");
-            rowNum++;
-        } else if (token[0] == '#') {
-            token = strtok(NULL, "\n");
-            rowNum++;
-        } else if (isKeyword(token)) {
-            printf("<KEYWORD : %s>\n", token);
-            addToSymbolTable(token, KEYWORD, rowNum, startCol);
-        } else if (isalpha(token[0]) || token[0] == '_') {
-            printf("<IDENTIFIER : %s>\n", token);
-            addToSymbolTable(token, IDENTIFIER, rowNum, startCol);
-        } else if (strchr(":()[],{}", token[0])) {
-            printf("<SPECIAL_SYMBOL : %s>\n", token);
-            addToSymbolTable(token, SPECIAL_SYMBOL, rowNum, startCol);
-        } else if (strcmp(token, "=") == 0 || strcmp(token, "+=") == 0) {
-            printf("<OPERATOR : %s>\n", token);
-            addToSymbolTable(token, OPERATOR, rowNum, startCol);
-        } else if (isdigit(token[0])) {
-            printf("<NUMERIC_CONSTANT : %s>\n", token);
-            addToSymbolTable(token, NUMERIC_CONSTANT, rowNum, startCol);
-        } else if (token[0] == '"' || token[0] == '\'') {
-            printf("<STRING_LITERAL : %s>\n", token);
-            addToSymbolTable(token, STRING_LITERAL, rowNum, startCol);
+        int tokenRow = row, tokenCol = col;
+
+        // Handle new lines
+        if (strchr(token, '\n')) {
+            row++;
+            col = 1;
+        } else {
+            col += strlen(token) + 1;
         }
 
-        colNum += strlen(token) + 1;
-        if (strchr(token, '\n')) {
-            rowNum++;
-            colNum = 1;
+        // Ignore comments
+        if (strncmp(token, "#", 1) == 0) {
+            token = strtok(NULL, "\n");
+            continue;
+        }
+
+        // Check for keywords
+        if (isKeyword(token)) {
+            printf("<KEYWORD : %s>\n", token);
+            addToSymbolTable(token, KEYWORD, tokenRow, tokenCol);
+        }
+        // Check for operators
+        else if (isOperator(token)) {
+            printf("<OPERATOR : %s>\n", token);
+            addToSymbolTable(token, OPERATOR, tokenRow, tokenCol);
+        }
+        // Check for numeric constants
+        else if (isdigit(token[0])) {
+            printf("<NUMERIC_CONSTANT : %s>\n", token);
+            addToSymbolTable(token, NUMERIC_CONSTANT, tokenRow, tokenCol);
+        }
+        // Check for string literals
+        else if (token[0] == '"' || (token[0] == 'f' && token[1] == '"')) {
+            printf("<STRING_LITERAL : %s>\n", token);
+            addToSymbolTable(token, STRING_LITERAL, tokenRow, tokenCol);
+        }
+        // Check for identifiers
+        else if (isalpha(token[0]) || token[0] == '_') {
+            printf("<IDENTIFIER : %s>\n", token);
+            addToSymbolTable(token, IDENTIFIER, tokenRow, tokenCol);
+        }
+        // Check for special symbols
+        else if (strchr(":()[],{}", token[0])) {
+            printf("<SPECIAL_SYMBOL: %s>\n", token);
+            addToSymbolTable(token, SPECIAL_SYMBOL, tokenRow, tokenCol);
         }
 
         token = strtok(NULL, " \t\n():,[]{}");
     }
 }
 
+// Print the symbol table
 void printSymbolTable() {
     printf("\n===== SYMBOL TABLE =====\n");
     printf("Index\tToken\t\tType\t\tRow\tCol\n");
@@ -111,12 +135,12 @@ void printSymbolTable() {
             case STRING_LITERAL: typeName = "STRING_LITERAL"; break;
             default: typeName = "UNKNOWN"; break;
         }
-        printf("%d\t%s\t\t%s\t\t%d\t%d\n", i, symbolTable[i].name, typeName, symbolTable[i].rowNum, symbolTable[i].colNum);
+        printf("%d\t%s\t\t%s\t\t%d\t%d\n", i, symbolTable[i].name, typeName, symbolTable[i].row, symbolTable[i].col);
     }
 }
 
 int main() {
-    FILE *fp = fopen("script.rb", "r");
+    FILE *fp = fopen("sample.rb", "r");
     if (!fp) {
         printf("Error opening file.\n");
         return 1;
